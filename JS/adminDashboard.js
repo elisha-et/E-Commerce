b8e46c4c464e02af9fb51f83b7cc79dc0c05e46d
hacker-elisha <etukudohelisha@gmail.com>
2024-02-22 20:07:27 +0100

    Added edit and delete button on the dashboard

--- JS/adminDashboard.js
+++ JS/adminDashboard.js
@@ -49,8 +49,10 @@ function displayProduct(){
          <p class = "pname">${product.name}</p>
          <p class = "pprice">$${product.price}</p>
          <p class = "pstock">Stock: ${product.quantity}</p>
-         <button onclick="editProduct('${product.name}')" class="editProduct">Edit Product</button>
-         <button onclick="deleteProduct('${product.name}')" class="deleteProduct">Delete Product</button>
+         <div class="button">
+         <button onclick="editProduct('${product.name}')" class="edit">Edit Product</button>
+         <button onclick="deleteProduct('${product.name}')" class="delete">Delete Product</button>
+         </div>
          `
  
  productList.appendChild(listItem)
@@ -59,33 +61,37 @@ function displayProduct(){
  
  displayProduct();
 
- function deleteEmployee(id){
-    const confirmAction = confirm("Are you sure you want to delete?");
+ function deleteProduct(name){
+    const confirmAction = confirm("Are you sure you want to delete the product?");
     if(confirmAction){
-        let employees = JSON.parse(localStorage.getItem("employees")) || [];
-        employees = employees.filter((employee)=>employee.id !== id);
-        localStorage.setItem("employees",JSON.stringify(employees));
-        displayEmployee();
+        let products = JSON.parse(localStorage.getItem("products")) || [];
+        products = products.filter((product)=>product.name !== name);
+        localStorage.setItem("products",JSON.stringify(products));
+        displayProduct();
     }
 }
 
-function editEmployee(id){
-    const confirmAction = confirm("Are you sure you want to make a change?");
+function editProduct(name){
+    const confirmAction = confirm("Are you sure you want to edit the product?");
     if(confirmAction){
-        let employees = JSON.parse(localStorage.getItem("employees")) || [];
-       const employee = employees.find((employee)=>employee.id === id);
-       if(employee){
-        const empName = prompt("enter the new employee name: ",employee.name);
-        const newPosition = prompt("enter the new employee position: ",employee.position);
+        let products = JSON.parse(localStorage.getItem("products")) || [];
+       const product = products.find((product)=>product.name === name);
+       if(product){
+        const newName = prompt("Enter the new product name: ",product.name);
+        const newQuantity = prompt("Enter the new product quantity: ",product.quantity);
+        const newPrice = prompt("Enter the new product price: ",product.price);
+        const newImage = prompt("Enter the new product image: ",product.image);
 
-        if(empName && newPosition){
-            employee.name = empName;
-            employee.position = newPosition;
-            localStorage.setItem("employees",JSON.stringify(employees));
-            displayEmployee();
+        if(newName && newQuantity && newPrice && newImage){
+            product.name = newName;
+            product.quantity = newQuantity
+            product.price = newPrice
+            product.image = newImage
+            localStorage.setItem("products",JSON.stringify(products));
+            displayProduct();
         }
        }else{
-        alert("No such employee")
+        alert("No such product found.")
        }
     }
 }
